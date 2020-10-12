5b85e71bdc0abfed8c1860ad2c926085a02949d9
mezza <gpwl8411@gmail.com>
2020-10-13 01:54:37 +0900

    채팅방별로 입장

--- onAirStudy/src/main/java/com/kh/onairstudy/chat/controller/ChatController.java
+++ onAirStudy/src/main/java/com/kh/onairstudy/chat/controller/ChatController.java
@@ -1,37 +1,38 @@
 package com.kh.onairstudy.chat.controller;
 
-import java.security.Principal;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 import com.kh.onairstudy.chat.model.service.ChatService;
 import com.kh.onairstudy.chat.model.vo.Chat;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RestController
+@Controller
 @Slf4j
-@RequestMapping
 public class ChatController {
 	
 	@Autowired
 	private ChatService chatService;
 	
 	// 채팅 메세지 전달
-    @MessageMapping("/app/{roomNo}")
+    @MessageMapping("/hello/{roomNo}")
     @SendTo("/subscribe/chat/{roomNo}")
-    public Chat sendChatMessage(@DestinationVariable String roomNo, Chat chat, Principal principal) {
-    	log.debug("chat={}",chat);
-    	
+    public Chat broadcasting(Chat chat) {
+//    	log.debug("room넘어오나?{}",roomNo);
+    	log.debug("?????????????gㅎ");
+    	log.debug("받아온 data={}",chat);
+//    	log.debug("chat={}",chat);
+//    	Chat chat = new Chat();
 //    	chat.setMessageType("");
         chat.setSendDate(new Date());
         
         return chat;
     }
+    
+    
 }
--- onAirStudy/src/main/java/com/kh/onairstudy/chat/web/WebSocketConfig.java
+++ onAirStudy/src/main/java/com/kh/onairstudy/chat/web/WebSocketConfig.java
@@ -2,19 +2,19 @@ package com.kh.onairstudy.chat.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@SuppressWarnings("deprecation")
+
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/subscribe");
-        config.setApplicationDestinationPrefixes("/");
+        config.setApplicationDestinationPrefixes("/app");
 
     }
     
