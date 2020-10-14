366c237e4d52a1c94e32ef204ffa8d58855472d0
godhy1023 <dy0113tenf@naver.com>
2020-10-14 20:41:07 +0900

    초대 리스트

--- onAirStudy/src/main/java/com/kh/onairstudy/invitation/model/service/InvitationService.java
+++ onAirStudy/src/main/java/com/kh/onairstudy/invitation/model/service/InvitationService.java
@@ -1,5 +1,11 @@
 package com.kh.onairstudy.invitation.model.service;
 
+import java.util.List;
+
+import com.kh.onairstudy.invitation.model.vo.Invitation;
+
 public interface InvitationService {
+
+	List<Invitation> selectInvitationList();
 
 }
--- onAirStudy/src/main/java/com/kh/onairstudy/invitation/model/vo/Invitation.java
+++ onAirStudy/src/main/java/com/kh/onairstudy/invitation/model/vo/Invitation.java
@@ -1,17 +1,32 @@
 package com.kh.onairstudy.invitation.model.vo;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString
 
 public class Invitation implements Serializable{
 
+	private int no;
+	private String hostingId;
+	private String invitiedId;
+	private int srNo;
+	private Date invitationDate;
+	private char invitationYN;
+	
+	private String srTitle;
+	private String srGoal;
+	
+	private String catetory;
+	
 }
