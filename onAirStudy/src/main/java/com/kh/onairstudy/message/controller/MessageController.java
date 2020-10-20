53983e331fb5ed2a866947fc75f86510e209569c
mezza <gpwl8411@gmail.com>
2020-10-20 21:44:00 +0900

    message selectList,controller, vo , model update and connect mypage1

--- onAirStudy/src/main/java/com/kh/onairstudy/message/controller/MessageController.java
+++ onAirStudy/src/main/java/com/kh/onairstudy/message/controller/MessageController.java
@@ -1,10 +1,17 @@
 package com.kh.onairstudy.message.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.onairstudy.member.model.vo.Member;
 import com.kh.onairstudy.message.model.service.MessageService;
+import com.kh.onairstudy.message.model.vo.Message;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,4 +22,12 @@ public class MessageController {
 
 	@Autowired
 	private MessageService messageService;
+	
+	@RequestMapping("/message/messageList.do")
+	public String messageList(HttpSession session,Model model) {
+		Member member = (Member)session.getAttribute("loginMember");
+		List<Message> messageList = messageService.selectMsgList(member.getMemberId());
+		model.addAttribute("messageList", messageList);
+		return "message/messageList";
+	}
 }
--- onAirStudy/src/main/java/com/kh/onairstudy/message/model/service/MessageService.java
+++ onAirStudy/src/main/java/com/kh/onairstudy/message/model/service/MessageService.java
@@ -1,5 +1,11 @@
 package com.kh.onairstudy.message.model.service;
 
+import java.util.List;
+
+import com.kh.onairstudy.message.model.vo.Message;
+
 public interface MessageService {
+
+	List<Message> selectMsgList(String memberId);
 
 }
