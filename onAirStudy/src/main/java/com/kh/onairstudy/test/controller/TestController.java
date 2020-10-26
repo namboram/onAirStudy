a34a8d3e7fb2f2081e6b0bec84468e3005646b21
godhy1023 <dy0113tenf@naver.com>
2020-10-27 02:12:59 +0900

    question 등록 완료(?)

--- onAirStudy/src/main/java/com/kh/onairstudy/test/controller/TestController.java
+++ onAirStudy/src/main/java/com/kh/onairstudy/test/controller/TestController.java
@@ -2,18 +2,16 @@ package com.kh.onairstudy.test.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,11 +35,17 @@ public class TestController {
 	}
 	
 	@RequestMapping(value = "mypage2/testquestion.do", method = RequestMethod.POST)
-	public String testQuestion( Test test, 
-			@RequestParam(value = "upFile", required = false) MultipartFile upFile, RedirectAttributes redirectAttr,
-			HttpServletRequest request) throws IllegalStateException, IOException{
+	public String testQuestion( Test test, 	@RequestParam(value = "upFile", required = false) MultipartFile upFile,
+								@RequestParam Map<String, String> param, @RequestParam("testAnswer") int answer, RedirectAttributes redirectAttr, 
+								HttpServletRequest request) throws IllegalStateException, IOException{
 		
-						
+		String question = request.getParameter("testQeustion");
+		String c1 = request.getParameter("testChoice_1");
+		String c2 = request.getParameter("testChoice_2");
+		String c3 = request.getParameter("testChoice_3");
+		String c4 = request.getParameter("testChoice_4");
+		
+		
 		String saveDirectory = request.getServletContext().getRealPath("/resources/testPic");
 		String renamedFilename = Utils.getRenamedFileName(upFile.getOriginalFilename());
 //		if(upFile.isEmpty()) continue;
@@ -50,13 +54,18 @@ public class TestController {
 		upFile.transferTo(dest);
 		
 		test.setFilePath(saveDirectory);
-		test.setRenamedFilename(renamedFilename);		
-		
+		test.setRenamedFilename(renamedFilename);
+		test.setTestAnswer(answer);
+		test.setTestQuestion(question);
+		test.setTestChoice_1(c1);
+		test.setTestChoice_2(c2);
+		test.setTestChoice_3(c3);
+		test.setTestChoice_4(c4);		
 		int result = testService.insertQuestion(test);
 		
-		redirectAttr.addFlashAttribute("msg", "문제를 등록하였습니다.");
+		redirectAttr.addFlashAttribute("msg", result>0 ? "문제를 등록하였습니다." :"문제 등록 중 오류가 발생했습니다.");
 		
-		return "redirect:/test/questionForm.do";
+		return "redirect:/mypage2/mypage2_question.do";
 	}
 	
 	@RequestMapping("mypage2/pretest.do")
